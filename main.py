819b345e4f082c50aa1edf02f59d1ada2a41ca7a
katienkim <katie.kim@megazone.com>
2025-08-26 23:37:52 -0700

    created rest api stack with all of the necessary aws services and made boto3 lambda function

---
+++ main.py
@@ -0,0 +1,77 @@
+from aws_cdk import (
+    Stack,
+    aws_apigateway,
+    aws_lambda,
+    aws_s3 as s3
+)
+from constructs import Construct
+# CDK Labs library import for bedrock implementation
+from cdklabs.generative_ai_cdk_constructs import (
+    bedrock
+)
+
+class PyRestApiStack(Stack):
+    def __init__(self, scope = Construct, construct_id = str, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Create s3 bucket that stores HR related documents
+        s3_bucket = s3.Bucket(
+            self,
+            "HRDocsBucket",
+            public_read_access=False,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        # Create s3 vector bucket that stores chunks of embedded HR document data
+        s3_vector = s3.Bucket(
+            self,
+            "HRVectorBucket"
+        )
+
+        # Create knowledge base using Bedrock
+        # Use Bedrock Titan as the embedding model
+        # Use existing s3 vectorstore as the vector store for knowledge base
+        knowledge_base = bedrock.VectorKnowledgeBase(
+            self,
+            "HRKnowledgeBase",
+            embeddings_model=bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V2_1024,
+            vector_store=s3_vector
+        )
+
+        # Attach existing s3 document bucket as data source for knowledge base
+        knowledge_base.add_s3_data_source(
+            bucket=s3_bucket,
+            chunking_strategy= bedrock.ChunkingStrategy.semantic(
+                buffer_size=0,
+                breakpoint_percentile_threshold=95,
+                max_tokens=300
+            )
+        )
+
+        claude_model = bedrock.BedrockFoundationModel.ANTHROPIC_CLAUDE_V2_1
+        
+        # Create lambda function 
+        hr_lambda = aws_lambda.Function(
+            self,
+            "HRLambda",
+            runtime=aws_lambda.Runtime.PYTHON_3_13,
+            code=aws_lambda.Code.from_asset("services"),
+            handler="index.handler",
+            environment={
+                "KNOWLEDGE_BASE_ID": knowledge_base.knowledge_base_id,
+                "MODEL_ID": claude_model.model_id
+            }
+        )
+
+        knowledge_base.grant_read(hr_lambda)
+        claude_model.grant_invoke(hr_lambda)
+
+        # Create api gateway and its subroute
+        api = aws_apigateway.RestApi(self, "hr-api")
+        hr_resource = api.root.add_resource("hr")
+
+        # Create lambda integration attach to api gateway
+        # Add get and post methods to the lambda function
+        hr_lambda_integration = aws_apigateway.LambdaIntegration(hr_lambda)
+        hr_resource.add_method("GET", hr_lambda_integration)
+        hr_resource.add_method("POST", hr_lambda_integration)
---
+++ services/index.py
@@ -0,0 +1,78 @@
+import json
+import boto3
+import os
+
+bedrock_agent_runtime = boto3.client('bedrock-agent-runtime')
+
+def lambda_handler(event, context):
+    # Log the incoming event for debugging
+    print(f"Received event: {json.dumps(event)}")
+
+    try:
+        KNOWLEDGE_BASE_ID = os.environ.get('KNOWLEDGE_BASE_ID')
+        MODEL_ID = os.environ.get('MODEL_ID')
+
+        if not KNOWLEDGE_BASE_ID or not MODEL_ID:
+            raise ValueError("Missing KNOWLEDGE_BASE_ID or MODEL_ID environment variables.")
+  
+        body = json.loads(event['body'])
+        user_query = body.get('query', '')
+
+        if not user_query:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Query parameter is missing.'})
+            }
+
+        print(f"Received query: {user_query}")
+        response = bedrock_agent_runtime.retrieve_and_generate(
+            input={
+                'text': user_query
+            },
+            retrieveAndGenerateConfiguration={
+                'type': 'KNOWLEDGE_BASE',
+                'knowledgeBaseConfiguration': {
+                    'knowledgeBaseId': KNOWLEDGE_BASE_ID,
+                    'modelArn': "arn:aws:bedrock:us-west-2::foundation-model/{MODEL_ID}"
+                }
+            }
+        )
+
+        # Extract the generated response text and source citations
+        answer = response['output']['text']
+        citations = []
+        
+        if 'citations' in response:
+            for citation in response['citations']:
+                if 'retrievedReferences' in citation:
+                    for ref in citation['retrievedReferences']:
+                        if 'content' in ref and 'text' in ref['content']:
+                            citations.append(ref['content']['text'])
+                       
+                        if 'location' in ref and 's3Location' in ref['location']:
+                             s3_loc = ref['location']['s3Location']
+                             citations.append(f"Source: s3://{s3_loc['uri']}") 
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*' 
+            },
+            'body': json.dumps({
+                'query': user_query,
+                'answer': answer,
+                'citations': citations
+            })
+        }
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({'error': str(e)})
+        }
